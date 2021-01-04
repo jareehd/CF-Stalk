20dc4d450efa4f57b552cd3bbcf19064ed127e03
DHEERAJ MAURYA <dheeraj945016@gmail.com>
2021-01-05 03:59:42 +0530

    added user rating count

--- src/components/user/user.js
+++ src/components/user/user.js
@@ -1,11 +1,30 @@
 import React from 'react'
 
 const User = (props) =>{
-    console.log(props.location.result)
+    
+    let Rating = new Map()
+            props.location.result.forEach( (e)=> {
+            const rating = e.problem.rating
+            if( !Rating.has(rating) ){
+                Rating.set(rating,1)
+            } 
+            else {
+                const prevCount = Rating.get(rating)
+                Rating.set( rating ,1 + prevCount)
+            } 
+    })
+    
+     
+    const length = props.location.result.length
     return (
         <div>
            user
-           
+           { length === 0 ? <p> User has no compitive record</p> :(
+               <div>
+                   ok
+                   {Rating.forEach((key,value) => console.log(key,value) )}
+               </div>
+           )}
         </div>
     )
 }
